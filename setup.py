8a1620124981c640c1f892b57619549f9974b5da
Michael Potter <mpotter83@gmail.com>
2011-02-07 00:32:02 -0500

    Added tutorials to distribution

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
-import sys
+import sys, os
 
 DATA_FILES = ['README', 'LICENSE']
 
 ASTEROID_PNGS = ['large.png', 'medium.png', 'small.png', 'ship.png']
 INVASION_PNGS = ['ship.png', 'alien.png']
+
+TUTORIALS = ['examples/tutorial/'+t for t in os.listdir('examples/tutorial/')]
+DATA_FILES += TUTORIALS
 
 setup(
 	name='pyrge', 
@@ -12,7 +15,9 @@ setup(
 	author="Michael Potter",
 	author_email="michael@potterpcs.net",
 	url="http://github.com/momikey/pyrge",
-	packages = ['pyrge', 'pyrge.examples.asteroid', 'pyrge.examples.invasion'],
+	packages = ['pyrge',
+                    'pyrge.examples.asteroid',
+                    'pyrge.examples.invasion'],
 	package_dir = {'pyrge': ''},
 	package_data = {'pyrge': DATA_FILES, 
 		'pyrge.examples.asteroid': ['*.png'],
